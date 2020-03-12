dcdde4dd583093d03bcc7e007869e7548a823e18
ginanjarn <ginanjarn@gmail.com>
2020-03-13 05:51:45 +0700

    * restructure code * fix formatting

--- godoc.py
+++ godoc.py
@@ -16,10 +16,14 @@ class GodocCommand(sublime_plugin.EventListener):
 		view.settings().set("word_separators","/\\()\"'-:,;<>~!@#$%^&*|+=[]{``~?")
 		region = view.word(point)
 		
-		doc_value = self.get_definition(view,region,point)
+		doc_value = self.get_definition(view,region)
+		if not doc_value:
+			doc_value = self.get_description(view,region)
 		if not doc_value:
 			return
-		self.show_popup(view,point,doc_value)
+
+		wrap_div = lambda x: "<div style=\"margin:1em\">{}</div>".format(x)
+		self.show_popup(view,point,wrap_div(doc_value))
 
 
 	def show_popup(self, view, location, value):
@@ -34,13 +38,14 @@ class GodocCommand(sublime_plugin.EventListener):
             max_width=600,
             max_height=400)
 		pass
+		
 
+	# fetch documentation for package.member
 	def fetch_doc(self,view,name):
 		env = GolangSettings()
-
 		working_dir = os.path.dirname(view.file_name())
 
-		gofmt = subprocess.Popen(["go","doc",name],
+		gofmt = subprocess.Popen(["go","doc","-short",name],
 			stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,env=env.get_environment(),cwd=working_dir)
 		sout, serr = gofmt.communicate()
 		# print(sout.decode())
@@ -48,54 +53,108 @@ class GodocCommand(sublime_plugin.EventListener):
 
 		return sout.decode()
 
-	def get_definition(self,view,region,point):
+
+	def get_description(self,view,region):
 		env = GolangSettings()
 		source = "{}:#{},#{}".format(view.file_name(),region.begin(),region.end())
 		# print(source)
 		working_dir = os.path.dirname(view.file_name())
+
 		gofmt = subprocess.Popen(["guru","-json","describe",source],
-			# stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,env=env.get_environment())
 			stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,env=env.get_environment(),cwd=working_dir)
 		sout, serr = gofmt.communicate()
 		out_str = sout.decode()
 		if not out_str:
 			return
 
-		# print(out_str)
 		# JSON loads
-		# ret = json.loads(out_str)
-		ret = loads(out_str)
-		if not ret:
+		try:
+			ret = loads(out_str)
+			# print(ret)
+			if ret["detail"]=="value":
+				definition = ret["value"]["type"]
+				def_position = ret["value"].get("objpos",None)
+
+				doc_list = self.to_html(definition,def_position)
+				if not doc_list:
+					return
+					
+				doc_value = "".join(doc_list)
+
+				return doc_value
+
+			if ret["detail"]=="type":			
+				if "namedef" in ret["type"]:
+					definition = self.fetch_doc(view,ret["type"]["type"])
+				else:
+					definition = None
+					# definition = ret["type"]["type"]
+
+				def_position = ret["type"].get("namepos",None)
+
+				doc_list = self.to_html(definition,def_position)
+				if not doc_list:
+					return
+
+				doc_value = "".join(doc_list)				
+
+				return doc_value
+
+		except ValueError:
 			return
 
-		if ret["detail"]=="value":
-			doc_value = ret["value"]["type"]
+
+	def get_definition(self,view,region):
+		env = GolangSettings()
+		source = "{}:#{},#{}".format(view.file_name(),region.begin(),region.end())
+		# print(source)
+		working_dir = os.path.dirname(view.file_name())
+
+		working_dir = os.path.dirname(view.file_name())
+		gofmt = subprocess.Popen(["guru","-json","definition",source],
+			stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,env=env.get_environment(),cwd=working_dir)
+		sout, serr = gofmt.communicate()
+		out_str = sout.decode()
+		if not out_str:
+			return
+
+		# JSON loads
+		try:
+			ret = loads(out_str)
+			# print(ret)
+			definition = ret["desc"]
+			def_position = ret["objpos"]
+
+			doc_list = self.to_html(definition,def_position)
+			if not doc_list:
+				return
+					
+			doc_value = "".join(doc_list)
+
 			return doc_value
+		except ValueError:
+			pass
 
-		if ret["detail"]=="type":			
-			if "namedef" in ret["type"]:
-				definition = self.fetch_doc(view,ret["type"]["type"])
-			else:
-				definition = ret["type"]["type"]
 
-			def_list = definition.split("\n")
+	def to_html(self,definition,def_position=None):
 
-			def_position = ret["type"].get("namepos",None)
-			if def_position:
-				def_list.append("<a href=\"{}\">Go to definition.</a>".format(def_position))
-			# def_list.append("<a href=\"{}\">Go to definition.</a>".format(ret["type"]["namepos"]))
+		if not definition:
+			return
 
-			html_tab = lambda s : s.replace("\t","&nbsp;&nbsp;&nbsp;&nbsp;")
-			html_p = lambda s : "<p>{}</p>".format(s)
+		def_list = definition.split("\n")
 
-			def_list = [html_tab(i) for i in def_list]
-			def_list = [html_p(i) for i in def_list]
+		if def_position:
+			def_list.append("<a href=\"{}\">Go to definition.</a>".format(def_position))
+		
+		html_tab = lambda s : s.replace("\t","&nbsp;&nbsp;&nbsp;&nbsp;")
+		html_p = lambda s : "<p>{}</p>".format(s)
 
-			doc_value = "".join(def_list)
-			
-			return doc_value
+		def_list = [html_tab(i) for i in def_list]
+		def_list = [html_p(i) for i in def_list]
+		
+		return def_list
 
 	def goto_definition(self,view,name):
 		view.window().open_file(name,sublime.ENCODED_POSITION)
-		pass
+		# pass
 		
